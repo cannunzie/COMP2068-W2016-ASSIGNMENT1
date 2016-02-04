866f5e7e244333a9d2e93c58739bf83aae82f067
Caleb Annunziello <cannunziello@hotmail.com>
2016-02-03 20:39:16 -0500

    BRIGTHO SAVES THE WORLD

--- routes/index.js
+++ routes/index.js
@@ -6,19 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/', function(req, res, next) {
+router.get('/project', function(req, res, next) {
   res.render('project', { title: 'project' });
 });
 
-router.get('/', function(req, res, next) {
+router.get('/services', function(req, res, next) {
   res.render('services', { title: 'services' });
 });
 
-router.get('/', function(req, res, next) {
+router.get('/about', function(req, res, next) {
   res.render('about', { title: 'about' });
 });
 
-router.get('/', function(req, res, next) {
+router.get('/contact', function(req, res, next) {
   res.render('contact', { title: 'contact' });
 });
 
